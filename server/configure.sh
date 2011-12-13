38a8081d5e55faf798dba1d2e0ac116fa41e34d0
Colin Sullivan <colinsul@gmail.com>
2011-12-13 00:36:52 -0800

    Updated makefiles and configure to new boost paths.

--- server/configure.sh
+++ server/configure.sh
@@ -5,15 +5,15 @@ echo "************************"
 echo "*    Building boost    *"
 echo "************************"
 
-cd ./vendor/boost_1_47_0/
+cd ./vendor/boost/
 ./bootstrap.sh --prefix=$(PWD)
 ./b2 install
 cd ../../
-ln -s ./vendor/boost_1_47_0/lib/libboost_system.dylib ./libboost_system.dylib
-ln -s ./vendor/boost_1_47_0/lib/libboost_date_time.dylib ./libboost_date_time.dylib
-ln -s ./vendor/boost_1_47_0/lib/libboost_regex.dylib ./libboost_regex.dylib
-ln -s ./vendor/boost_1_47_0/lib/libboost_random.dylib ./libboost_random.dylib
-ln -s ./vendor/boost_1_47_0/lib/libboost_program_options.dylib ./libboost_program_options.dylib
+ln -s ./vendor/boost/lib/libboost_system.dylib ./libboost_system.dylib
+ln -s ./vendor/boost/lib/libboost_date_time.dylib ./libboost_date_time.dylib
+ln -s ./vendor/boost/lib/libboost_regex.dylib ./libboost_regex.dylib
+ln -s ./vendor/boost/lib/libboost_random.dylib ./libboost_random.dylib
+ln -s ./vendor/boost/lib/libboost_program_options.dylib ./libboost_program_options.dylib
 
 echo "************************"
 echo "*      Boost done      *"
@@ -24,8 +24,8 @@ echo "* Building websocketpp *"
 echo "************************"
 
 cd ./vendor/zaphoyd-websocketpp-ec28725/
-export CPLUS_INCLUDE_PATH=../boost_1_47_0/include:$CPLUS_INCLUDE_PATH
-export DYLD_LIBRARY_PATH=../boost_1_47_0/lib:$DYLD_LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=../boost/include:$CPLUS_INCLUDE_PATH
+export DYLD_LIBRARY_PATH=../boost/lib:$DYLD_LIBRARY_PATH
 make
 
 echo "************************"
